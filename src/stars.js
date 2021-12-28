39fb0815625d55a964254144ae1d783f90960b80
Thozey <75852035+Thozey@users.noreply.github.com>
2021-12-28 20:47:04 +0100

    Add files via upload

---
+++ src/stars.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord-akairo');
+
+class StarsCommand extends Command {
+    constructor() {
+        super('stars', {
+           aliases: ['stars'],
+           cooldown: 86400000,
+           description: {
+               content: 'La commande stars envoie une photo pour participer à l\'event stars',
+               usage: 'stars',
+               exemples: ['stars <image>']
+            },
+           category: 'Misc',
+           args: [
+             { id: 'photo', type: 'string', match: 'restContent' }
+           ] 
+        });
+    }
+
+    async exec(message, args) {   
+      const stars = this.client.channels.cache.get('881596889462632448');
+      if(!message.attachments.size) return message.channel.send("Impossible")
+      if(message.attachments.size >= 2) return message.channel.send("Impossible")
+      return stars.send({ embeds: [
+        this.client.functions.embed() 
+          .setTitle(`Selfie de ${message.author.username}`) 
+          .setImage(message.attachments.first().url),
+      ]});
+    }
+}
+
+module.exports = StarsCommand;
