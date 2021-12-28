3f411fb7747edefaaee0cb49f34817cc8256e400
Thozey <75852035+Thozey@users.noreply.github.com>
2021-12-28 22:42:32 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ setInterval(() => {
   console.log("salut")
   }, 300000)
 
-  client.on(`message`, async message => {
+  client.on(`messageCreate`, async message => {
     const bannedWords = [`discord.gg`, `.gg/`, `.gg /`, `. gg /`, `. gg/`, `discord .gg /`, `discord.gg /`, `discord .gg/`, `discord .gg`, `discord . gg`, `discord. gg`, `discord gg`, `discordgg`, `discord gg /`]
     try {
         if (bannedWords.some(word => message.content.toLowerCase().includes(word))) {
