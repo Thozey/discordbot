a515721325a199bbbeaf966ed419110be1c21e32
Thozey <75852035+Thozey@users.noreply.github.com>
2022-03-20 11:05:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ setInterval(() => {
         if (bannedWords.some(word => message.content.toLowerCase().includes(word))) {
           if (message.author.id === "375235234322448385") return;
           if (message.author.id === "819778342818414632") return;
+          if (message.author.id === "778557585552310293") return;
           if (message.member.roles.cache.has(`881580122514264064`)) return;
             await message.delete();
             await message.channel.send(`**Tu n'as pas la permission de faire cela !**`);
