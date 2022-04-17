addbd12d2f29bc8809df42ef2f244623fe093d46
Thozey <75852035+Thozey@users.noreply.github.com>
2022-04-17 12:05:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const salon = client.channels.cache.get("881587222829211658")
           if (message.author.id === "375235234322448385") return;
           if (message.author.id === "720012853788278866") return;
           if (message.author.id === "778557585552310293") return;
+          if (message.author.id === "689012956620783888") return;
           if (message.member.roles.cache.has(`881580122514264064`)) return;
             await message.delete();
             await message.channel.send(`**Tu n'as pas la permission de faire cela !**`);
